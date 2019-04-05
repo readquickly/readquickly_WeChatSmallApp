a6c686fe9bf7d6a11f826edfbbd86b33a28d3899
cdfmlr <cdfmlr@163.com>
2019-04-05 15:43:52 +0800

    Spider提供的函数升级为可以捕获错误的。

--- spiders/Spider.py
+++ spiders/Spider.py
@@ -10,29 +10,56 @@ class Spider(object):
             'Accept': 'application/json, text/plain, */*',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15'
     }
+    
+    def headers_set(self, special_headers):
+        self.headers.update(special_headers)
 
     def __init__(self, url):
             self.url = url
     
     def getPage(self, url):
-        response = requests.get(url, headers=self.headers)
-        return response
+        try:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                return response
+            else:
+                raise RuntimeError('Response Status Code != 200')
+        except Exception as e:
+            print('[getPage Error] ', e)
+            return None
 
     def getData(self, response):
-        data = response.text
-        return data
+        try:
+            if response:
+                data = response.text
+                return data
+            else:
+                raise RuntimeError('Response == None')
+        except Exception as e:
+            print('[getData Error] ', e)
+            return None
 
     def saveData(self, data):
-        print(data)
+        try:
+            if data:
+                print(data)
+            else:
+                raise RuntimeError('data == None')
+        except Exception as e:
+            print('[saveData Error] ', e)
+            return None       
+        
 
     def run(self):
-        page = self.getPage(self.url)
-        text = self.getData(page)
-        self.saveData(text)
+        try:
+            page = self.getPage(self.url)
+            text = self.getData(page)
+            self.saveData(text)
+            return True
+        except Exception as e:
+            print('[Spider.run Error], e')
+            return False
 
-
-    def headers_set(self, special_headers):
-        self.headers.update(special_headers)
 
 if __name__ == "__main__":
     s = Spider("http://www.baidu.com")
