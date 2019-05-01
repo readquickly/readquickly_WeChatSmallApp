d3ac8ac9f5c8e092e2583067aab029c5ebdab783
cdfmlr <cdfmlr@163.com>
2019-05-01 22:01:24 +0800

    修改后端bug，添加手动写入数据的功能

--- Server/content.py
+++ Server/content.py
@@ -14,14 +14,13 @@ import sys
 
 from .weather import weather
 from .spider import newsDatabase
+from .notice import noticesDatabase
 
 
 def getNews():
     name = newsDatabase.getDatabaseName()
     collection = newsDatabase.getDatabaseCollection()
-
     news = newsDatabase.readFromDatabase(name, collection)
-
     return news
 
 
@@ -30,12 +29,21 @@ def getWeather(city):
     return weatherBriefing
 
 
+def getNotice():
+    name = noticesDatabase.getDatabaseName()
+    collection = noticesDatabase.getDatabaseCollection()
+    notices = noticesDatabase.readFromDatabase(name, collection)
+    return notices
+
+
 def getContent(city):
     news = getNews()
     weatherBriefing = getWeather(city)
+    notices = getNotice()
 
     content = []
-    content.append(weatherBriefing)
-    content += news
+    content += notices or []
+    content += [weatherBriefing] or []
+    content += news or []
 
     return content
---
+++ Server/notice/__init__.py
@@ -0,0 +1,8 @@
+'''
+用来处理手动数据接入：
+
+* driver.py：手动消息接入的交互实现
+
+* superNotice.py：手动消息接口
+* noticesDatabase.py：数据库读写接口
+'''
